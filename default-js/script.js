d05d4a609471f3270cd3f089a8c86f2ec302afae
Firefly718 <System.BLA.16@yandex.ru>
2021-01-04 21:59:15 +0200

    finished about

---
+++ default-js/script.js
@@ -0,0 +1,9 @@
+// 11.2) Another script
+
+$(function(){
+  $('.slider').slick({
+    autoplay: true,
+    prevArrow: '<span class="prewArrow"><span>',
+    nextArrow: '<span class="nextArrow"><span>'
+  });
+ });
