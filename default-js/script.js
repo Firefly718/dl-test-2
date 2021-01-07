1f700b9904816953d28f1b4411dd126087ca6c06
Firefly718 <System.BLA.16@yandex.ru>
2021-01-07 18:26:41 +0200

    added burger-menu

--- default-js/script.js
+++ default-js/script.js
@@ -1,3 +1,4 @@
+// slider
 $(function(){
   $('.slider').slick({
     autoplay: true,
@@ -5,3 +6,19 @@ $(function(){
     nextArrow: '<span class="nextArrow"><span>'
   });
  });
+
+//  burger-menu (HEADER)
+$(document).ready(function(){
+  $("body > header > div > div > div").click(function(){
+    // console.log('есть клик');
+    $("body > header > div > div > nav").toggle('slow', 'swing');
+  });
+});
+
+// burger-menu (FOOTER)
+$(document).ready(function(){
+  $("#contacts > div > div.footer__menu > div").click(function(){
+    console.log('есть клик');
+    $("#contacts > div > div.footer__menu > nav").toggle('slow', 'swing');
+  });
+});
